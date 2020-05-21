8c0bb807f1d409da6f39c7a4f24a9f4f7befa78c
Prasanna Joshi <joshi.pras@husky.neu.edu>
2020-05-20 20:40:46 -0400

    added copyright

--- currency exchange rate tracker/ui.r
+++ currency exchange rate tracker/ui.r
@@ -15,13 +15,19 @@ shinyUI(pageWithSidebar(
   # Plot of moving averages
   mainPanel(
     textOutput("result"),
-    HTML("<ul><li>Track convergence and divergence of 30, 60 and 90 day moving averages of currency conversion rates.</li>
-              <li>Moving Average Convergence Divergence is a technical indicator developed by Gerald Appel in the late 1970s. It is supposed to reveal changes in the strength, direction, momentum, and duration of a trend in a time series.</li>
+    HTML("<ul>
+              <li>This app can be used to track convergence and divergence of 30, 60 and 90 day moving averages of currency conversion rates.</li>
+              <li> <a href = \"https://en.wikipedia.org/wiki/MACD\">Moving Average Convergence Divergence</a> is a technical indicator developed by Gerald Appel in the late 1970s. It is supposed to reveal changes in the strength, direction, momentum, and duration of a trend in a time series.</li>
               <li>This app tracks moving averages of currency conversion rates to detect strong trend changes and to indicate momentum.</li>         
               <li>The idea is to track the relationship between moving averages of the currency conversion rates for time periods of 30 days, 60 days and 90 days.</li>
               <li>As the shorter term moving average diverges further from the longer term moving average it indicates that the upside momentum is increasing. Divergence signals the end of momentum or current trend.</li>
          </ul>"),
     verbatimTextOutput("summary"),
-    plotOutput("maPlot")
+    plotOutput("maPlot"),
+    HTML("
+          <hr>
+          <ul>
+              <li style=\"color:#A0A0A0\"> Copyright Prasanna Joshi, 2017. All rights reserved.
+         </ul>")
   )
 ))
