766cc54edff113bb8c382ecd48ed6efefe89d6e3
Prasanna Joshi <joshi.pras@husky.neu.edu>
2020-05-21 21:59:57 -0400

    added messages printed upon convergence

--- currency exchange rate tracker/server.r
+++ currency exchange rate tracker/server.r
@@ -13,7 +13,7 @@ library(shinytoastr)
 # setwd("~/Documents/extras/github-projects/useful shiny applications/currency exchange rate tracker/currency-exchange-rate-tracker2")
 
 shinyServer(function(input, output) {
-  # toastr_success("Application Started")
+  #toastr_success("Application Started")
   
   output$maPlot <- renderPlot({
     
@@ -29,6 +29,7 @@ shinyServer(function(input, output) {
           verbose = FALSE,
           warning = TRUE,
           auto.assign = TRUE)
+
     curr.time.series <- setDT(as.data.frame(get(paste0("USD",input$currency))), keep.rownames = TRUE)[]
     colnames( curr.time.series) <- c("Date","Rate")
     curr.time.series <-  curr.time.series %>% arrange(desc(Date))
@@ -40,6 +41,8 @@ shinyServer(function(input, output) {
     curr.time.series.90.ma <- data.frame(curr.time.series$Date[1:30], as.data.frame(rollmean( curr.time.series$Rate, 90))[1:30,]) 
     moving.averages <- data.frame( curr.time.series.abs[,1], curr.time.series.30.ma[,2],  curr.time.series.60.ma[,2],  curr.time.series.90.ma[,2],  curr.time.series.abs[,2]) 
     colnames(moving.averages) <- c("Date", "30 Day", "60 Day", "90 Day","Current")
+    
+
     
     # line plots of moving averages
     ma.plot <- ggplot(data=moving.averages, 
@@ -55,6 +58,35 @@ shinyServer(function(input, output) {
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       theme(axis.title=element_text(face="bold", size="12", color="darkgreen"))
 
+    
+    
+    
     plot(ma.plot)
+    
+    # moving averages values from 1 day ago
+    y_30d <- moving.averages[2,'30 Day']
+    y_60d <- moving.averages[2,'60 Day']
+    y_90d <- moving.averages[2,'90 Day']
+    
+    # moving averages values from current day 
+    t_30d <- moving.averages[1,'30 Day']
+    t_60d <- moving.averages[1,'60 Day']
+    t_90d <- moving.averages[1,'90 Day']
+    
+    # If crossover occured, print conclusion
+    if(y_30d < y_60d && t_30d >= t_60d){
+      output$conclusion <- renderUI({"<b>Cross-Over occurred, upside momentum is increasing</b>"})
+      } 
+    
+    if(y_30d < y_90d && t_30d >= t_90d){
+      output$conclusion <- renderUI({print("<b>Cross-Over occurred, upside momentum is increasing</b>")})
+      } 
+    
+    if(y_60d < y_90d && t_60d >= t_90d){
+      output$conclusion <- renderUI({HTML("<b>Cross-Over occurred, upside momentum is increasing</b>")})
+      } 
+    
     })
 })
+
+
